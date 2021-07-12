16094e4e4d08ffd670133b02e00c7cf07a0e648f
Teodor Heggelund <teodor+git@iterate.no>
2021-07-12 20:21:30 +0200

    brr

--- src/teod/subcons/build.clj
+++ src/teod/subcons/build.clj
@@ -1,5 +1,33 @@
 (ns teod.subcons.build
-  (:require [hiccup.core :as h]))
+  (:require [hiccup.core]
+            [clojure.edn :as edn]
+            [clojure.string :as string]))
+
+;; Idea: hit a SINGLE use case in a good way first. That means finding
+;; theory-meaning/index.edn, and transforming it to index.html.
+
+(defn edn-paths []
+  ["theory-meaning/index.edn"])
+
+(defn build-edn [path]
+  (let [edn (-> path slurp edn/read-string)
+        html (hiccup.core/html edn)
+        html-path (string/replace path #".edn$" ".html")]
+    (spit html-path
+          (str "<!doctype html>"
+               "\n"
+               html))))
+
+(comment
+  (let [path "theory-meaning/index.edn"]
+    (string/replace path #".edn$" ".html")))
 
 (defn build-all [_opts]
-  (println "building it all"))
+  (doseq [path (edn-paths)]
+    (println "building" path)
+    (build-edn path)))
+
+
+(comment
+  (build-all {})
+  )
