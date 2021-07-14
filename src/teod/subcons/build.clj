8f5af9f5302c43ccea1ea7f189c34db77659662e
Teodor Heggelund <teodor+git@iterate.no>
2021-07-14 11:38:56 +0200

    brr

--- src/teod/subcons/build.clj
+++ src/teod/subcons/build.clj
@@ -3,29 +3,31 @@
             [clojure.edn :as edn]
             [clojure.string :as string]))
 
-;; Idea: hit a SINGLE use case in a good way first. That means finding
-;; theory-meaning/index.edn, and transforming it to index.html.
-
-;; Could look for EDN files, but right now I just have one. Later, perhaps.
 (defn edn-paths []
   ["theory-meaning/index.edn"])
 
-(defn build-edn [path]
-  (let [edn (-> path slurp edn/read-string)
-        html (hiccup.core/html edn)
-        html-path (string/replace path #".edn$" ".html")]
+(defn hiccup-html
+  "Write HTML from EDN with Hiccup"
+  [edn edn-path]
+  (let [html (hiccup.core/html edn)
+        html-path (string/replace edn-path #".edn$" ".html")]
     (spit html-path
           (str "<!doctype html>"
                "\n"
                html))))
 
 (defn build-all [_opts]
-  (doseq [path (edn-paths)]
-    (print "building" path "...")
-    (build-edn path)
+  (doseq [edn-path (edn-paths)]
+    (print "building" edn-path "...")
+    (let [edn (-> edn-path slurp edn/read-string)]
+      (hiccup-html edn edn-path))
     (println " Done.")))
-
 
 (comment
   (build-all {})
+
+  (meta
+   (-> (edn-paths)
+       first slurp edn/read-string))
+
   )
