f77d6da5af04e876b74dc7d46a32e1352424b0d5
Teodor Heggelund <git@teod.eu>
2021-12-26 22:20:28 +0100

    Think, clarify

--- src/teod/subcons/builder.clj
+++ src/teod/subcons/builder.clj
@@ -1,14 +1,24 @@
 (ns teod.subcons.builder
-  "A builder takes EDN and produces an output file."
+  "A builder takes EDN and produces an output file.
+
+  EDN source files pick their builder via metadata."
   (:require [hiccup2.core]
             [clojure.string :as str]))
 
-;; 2021-12-26 - Hunch that this does not deserve its own namespace.
+;; Note - builder is a bad word. To wide.
 ;;
-;; Right now - there's a multimethod with a single implementation.
+;; There are different kinds of things:
+;;
+;;  - File producers (like this one)
+;;  - Data transformers (like the EDN thing)
+;;
+;; That a file producer is a builder is perhaps obvious.
 
 (defmulti builder (fn [edn]
                     (-> edn meta :teod.subcons/builder)))
+
+;; Currently - the only builder we have takes Hiccup (EDN file) and produces an
+;; HTML file.
 
 (defmethod builder :teod.subcons.builder/hiccup-html [edn]
   (let [edn-path (-> edn meta :teod.subcons/source-path)
@@ -21,7 +31,8 @@
                html))))
 
 (defmethod builder :default [edn]
-  (println "no builder registered for" (-> edn meta :teod.subcons/builder)))
+  (println "no builder registered for" (-> edn meta :teod.subcons/builder))
+  (println " => no action taken."))
 
 (comment
   ;; multimethods can be a hassle in the REPL - just onload the whole ns.
