f2ea72851c43529db76fd1d27d9cbd75f6568438
Teodor Heggelund <git@teod.eu>
2021-12-26 22:33:20 +0100

    BIG CHANGE

--- src/teod/subcons/filewriter.clj
+++ src/teod/subcons/filewriter.clj
@@ -5,22 +5,16 @@
   (:require [hiccup2.core]
             [clojure.string :as str]))
 
-;;; builder is a bad word. To wide. Can we do better?
+;; Write different kinds of files.
 ;;
-;; There are different kinds of things:
+;; Provide file writer via EDN metadata:
 ;;
-;;  - File producers (like this one)
-;;  - Data transformers (like the EDN thing)
 ;;
-;; That a file producer is a builder is perhaps obvious.
 
 (defmulti write (fn [edn]
-                    (-> edn meta :teod.subcons/builder)))
+                    (-> edn meta :teod.subcons/filewriter)))
 
-;; Currently - the only builder we have takes Hiccup (EDN file) and produces an
-;; HTML file.
-
-(defmethod write :teod.subcons.builder/hiccup-html [edn]
+(defmethod write :teod.subcons.filewriter/hiccup-html [edn]
   (let [edn-path (-> edn meta :teod.subcons/source-path)
         html (hiccup2.core/html edn)
         html-path (str/replace edn-path #"\.edn$" ".html")]
