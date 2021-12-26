097768f6a5d7a3055f4cd036e626e940589b2339
Teodor Heggelund <git@teod.eu>
2021-12-26 20:06:39 +0100

    SCI

--- src/teod/subcons/sci.clj
+++ src/teod/subcons/sci.clj
@@ -1,2 +1,6 @@
 (ns teod.subcons.sci
-  (:refer-clojure :exclude [eval]))
+  (:refer-clojure :exclude [eval])
+  (:require [sci.core :as sci]))
+
+(defn eval [form]
+  (sci/eval-form (sci/init {}) form))
