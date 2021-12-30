274f472006d25e296e29e7abf24111c0f3427b59
Teodor Heggelund <git@teod.eu>
2021-12-30 18:55:12 +0100

    rethink

--- src/teod/subcons/pandoc.clj
+++ src/teod/subcons/pandoc.clj
@@ -9,24 +9,28 @@
   (assert (#{"markdown"} format)) ; whitelist - avoid errors
   (-> (sh "pandoc" "-s" "-f" format "-t" "json" :in source)
       :out
-      json/read-str
-      (get "blocks")))
+      (json/read-str :key-fn keyword)
+      :blocks))
 
 ;; At some point, the output of -parse needs to become hiccup.
+;;
+;; Any advice on how to approach this? Just do it by hand with clojure.walk? Not
+;; sure if it's a plain mapping (dispatch on :t) or if I'm going to have to
+;; "join" elements. Like how pandoc splits on space.
 
 (-parse {:source "# okay
 
 - are you okay?
 - sure?"
          :format "markdown"})
-;; => [{"t" "Header", "c" [1 ["okay" [] []] [{"t" "Str", "c" "okay"}]]}
-;;     {"t" "BulletList",
-;;      "c"
-;;      [[{"t" "Plain",
-;;         "c"
-;;         [{"t" "Str", "c" "are"}
-;;          {"t" "Space"}
-;;          {"t" "Str", "c" "you"}
-;;          {"t" "Space"}
-;;          {"t" "Str", "c" "okay?"}]}]
-;;       [{"t" "Plain", "c" [{"t" "Str", "c" "sure?"}]}]]}]
+;; => [{:t "Header", :c [1 ["okay" [] []] [{:t "Str", :c "okay"}]]}
+;;     {:t "BulletList",
+;;      :c
+;;      [[{:t "Plain",
+;;         :c
+;;         [{:t "Str", :c "are"}
+;;          {:t "Space"}
+;;          {:t "Str", :c "you"}
+;;          {:t "Space"}
+;;          {:t "Str", :c "okay?"}]}]
+;;       [{:t "Plain", :c [{:t "Str", :c "sure?"}]}]]}]
