6b2694b11b7e27a8721d1325ba6686dc25e34fd6
Teodor Heggelund <git@teod.eu>
2021-12-31 11:03:49 +0100

    Org example

--- src/teod/subcons/pandoc.clj
+++ src/teod/subcons/pandoc.clj
@@ -6,7 +6,7 @@
 (defn -parse [{:keys [source format]}]
   (assert source)
   (assert format)
-  (assert (#{"markdown"} format)) ; whitelist - avoid errors
+  (assert (#{"markdown" "org"} format)) ; whitelist - avoid errors
   (-> (sh "pandoc" "-f" format "-t" "json" :in source)
       :out
       (json/read-str :key-fn keyword)
@@ -23,6 +23,23 @@
 - are you okay?
 - sure?"
          :format "markdown"})
+;; => [{:t "Header", :c [1 ["okay" [] []] [{:t "Str", :c "okay"}]]}
+;;     {:t "BulletList",
+;;      :c
+;;      [[{:t "Plain",
+;;         :c
+;;         [{:t "Str", :c "are"}
+;;          {:t "Space"}
+;;          {:t "Str", :c "you"}
+;;          {:t "Space"}
+;;          {:t "Str", :c "okay?"}]}]
+;;       [{:t "Plain", :c [{:t "Str", :c "sure?"}]}]]}]
+
+(-parse {:source "* okay
+
+- are you okay?
+- sure?"
+         :format "org"})
 ;; => [{:t "Header", :c [1 ["okay" [] []] [{:t "Str", :c "okay"}]]}
 ;;     {:t "BulletList",
 ;;      :c
