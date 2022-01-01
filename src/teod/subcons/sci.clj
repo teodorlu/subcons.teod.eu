729d6c33fd0b3ffb502d8c4417009cd7956a4026
Teodor Heggelund <git@teod.eu>
2022-01-01 14:29:29 +0100

    Provide functions to sci env

--- src/teod/subcons/sci.clj
+++ src/teod/subcons/sci.clj
@@ -2,5 +2,9 @@
   (:refer-clojure :exclude [eval])
   (:require [sci.core :as sci]))
 
+(defn ctx []
+  (sci/init {:namespaces {'teod {'link (fn [x] [:a {:href x} x] )}}}))
+
 (defn eval [form]
-  (sci/eval-form (sci/init {}) form))
+  (sci/eval-form (ctx)
+                 form))
