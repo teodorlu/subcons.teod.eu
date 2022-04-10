b0e174468ed77271d93b743d4cb9a496350d0441
Teodor Heggelund <git@teod.eu>
2022-04-10 22:49:32 +0200

    Vendoc pandoc stuff

---
+++ how-to-learn-unix-edition/teod/pandoc.clj
@@ -0,0 +1,73 @@
+(ns teod.pandoc
+  "Pandoc-powered parsing and hiccup generation. Shells out to pandoc."
+  (:require
+   [clojure.data.json :as json]
+   [clojure.java.shell :refer [sh]]))
+
+(defn -parse [{:keys [source format]}]
+  (assert source)
+  (assert format)
+  (assert (#{"markdown" "org"} format) "Currently supported format are markdown and org-mode.")
+  (-> (sh "pandoc" "-f" format "-t" "json" :in source)
+      :out
+      (json/read-str :key-fn keyword)
+      :blocks))
+
+(defn org-> [source]
+  (-parse {:source source
+           :format "org"}))
+
+(defn markdown-> [source]
+  (-parse {:source source
+           :format "markdown"}))
+
+(defn header?
+  "Validates a pandoc 3-arity header - level, meta, content"
+  [el]
+  (and (= "Header" (:t el))
+       (<= 3 (count (:c el)))
+       (let [level (get (:c el) 0)]
+         (<= 1 level 6))))
+
+(defn ->hiccup* [form opts]
+  (cond (= (:t form)
+           "Para")
+        (into [:p]
+              (map #(->hiccup* % opts) (:c form)))
+
+        (= (:t form)
+           "Str")
+        (:c form)
+
+        (= (:t form)
+           "Plain")
+        (into [:span]
+              (map #(->hiccup* % opts) (:c form)))
+
+        (= (:t form)
+           "Space")
+        " "
+
+        (= (:t form)
+           "Emph")
+        (into [:em]
+              (map #(->hiccup* % opts) (:c form)))
+
+        (= (:t form)
+           "BulletList")
+        (into [:ul]
+              (for [raw-list-item (:c form)]
+                (into [:li]
+                      (map #(->hiccup* % opts)
+                           raw-list-item))))
+
+        ;; otherwise -- don't return!
+        :else
+        nil))
+
+(defn ->hiccup
+  ([data] (->hiccup data {}))
+  ([data opts]
+   (into [:div]
+         (for [p data]
+           (->hiccup* p opts)))))
---
+++ how-to-learn-unix-edition/teod/pandoc_test.clj
@@ -0,0 +1,38 @@
+(ns teod.pandoc-test
+  (:require [teod.pandoc :as sut]
+            [clojure.test :as t]))
+
+(t/deftest org->hiccup-test
+  ;; Are er skikkelig fin -- den lar oss skrive par med input og output, uten å
+  ;; styre med is, = funksjonskall og sånn. Anbefalt!
+  (t/are [pandoc hiccup] (= hiccup
+                            (sut/->hiccup* pandoc {}))
+    {:t "Para", :c [{:t "Str", :c "Paragraph"}]}
+    [:p "Paragraph"]
+
+    {:t "Space"}
+    " "
+
+    {:t "Str", :c "A plain string"}
+    "A plain string"
+
+    {:t "Emph", :c [{:t "Str", :c "two"}]}
+    [:em "two"]
+
+    {:t "Para",
+     :c
+     [{:t "Str", :c "Para"}
+      {:t "Space"}
+      {:t "Emph", :c [{:t "Str", :c "two"}]}
+      {:t "Str", :c "."}]}
+    [:p "Para" " " [:em "two"] "."]
+
+    {:t "BulletList",
+     :c
+     [[{:t "Plain", :c [{:t "Str", :c "Item"}]}]
+      [{:t "Plain",
+        :c [{:t "Str", :c "Another"} {:t "Space"} {:t "Str", :c "item"}]}]]}
+    [:ul
+     [:li [:span "Item"]]
+     [:li [:span "Another" " " "item"]]]
+    ))
