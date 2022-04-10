97786674055cbbfb47ffb4681a4e17b2b07ceeb3
Teodor Heggelund <git@teod.eu>
2022-04-10 22:42:55 +0200

    thinking

---
+++ how-to-learn-unix-edition/teod/how_to_learn_unix_edition.clj
@@ -0,0 +1,9 @@
+(ns teod.how-to-learn-unix-edition)
+
+;; Now .. I could just require in the standard stuff
+;;
+;; I already have access to the subcons pandoc stuff. And ... perhaps ..
+;; namespacing straight under teod is better than going through a proxy such as
+;; subcons or reflect
+;;
+;; 🤔
